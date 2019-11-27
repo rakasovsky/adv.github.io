3641de5ee65bef10ae8d444397ce55eb982c2599
rakasovsky <rakasovsky1@gmail.com>
2019-11-27 16:57:53 +0200

    test
    test1

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var  gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+const imagemin = require("gulp-imagemin");
+const webp = require("imagemin-webp");
+const extReplace = require("gulp-ext-replace");
+
+
+function f(done) {
+    gulp.src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(autoprefixer())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream())
+    done();
+}
+
+function sync(done) {
+    browserSync.init({
+        server: {
+            baseDir:'./'
+        },
+        port: 3333
+    })
+    done()
+}
+
+function browserReload(done) {
+    browserSync.reload();
+    done();
+}
+
+gulp.task("exportWebP", function() {
+    let src = "src/raw img/**/*.png"; // Where your PNGs are coming from.
+    let dest = "src/img"; // Where your WebPs are going.
+
+    return gulp.src(src)
+        .pipe(imagemin([
+            webp({
+                quality: 90,
+                // lossless: true
+            })
+        ]))
+        .pipe(extReplace(".webp"))
+        .pipe(gulp.dest(dest));
+});
+
+
+function watchFiles(){
+    gulp.watch('./scss/**/*', f)
+    gulp.watch('./**/*.html', browserReload);
+    gulp.watch('./**/*.js', browserReload);
+}
+
+
+
+gulp.task('default', gulp.parallel(sync,watchFiles));
+
+
---
+++ src/script/main.js
@@ -0,0 +1,201 @@
+jQuery(document).ready(function($){
+
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight){
+
+            $('header').removeClass('nav-down').addClass('nav-up');
+        } else {
+
+            if(st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up').addClass('nav-down');
+                $('.dropdown_wrapper').slideUp(500);
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+    // HEADER SUBMENU
+    $('.dropbtn').on({
+        'click': function () {
+            $('.dropdown_wrapper').slideToggle(500).css('display', 'flex');
+            $('.header_wrapper').css('background-color', 'rgba(23,23,23,1)');
+            $(this).toggleClass('arrowDown arrowUp');
+        }
+    });
+
+
+    // Header dropdown hide
+    // $(function ($) {
+    //     $(document).mouseup(function (e) {
+    //         let div = $('.dropdown_wrapper');
+    //         let header = $('.header_wrapper');
+    //         if (!div.is(e.target) && div.has(e.target).length === 0 ){
+    //             div.hide();
+    //             header.css('background-color', 'transparent')
+    //         }
+    //     })
+    // });
+
+    //Mobile menu + category list
+    $(document).ready(function() {
+        $('.dropbtn2').on('click', f_drpd);
+    });
+
+    function f_drpd(){
+        $('.dropdown_container').not($(this).next()).slideUp(500);
+        $(this).next().slideToggle(500);
+    }
+
+
+    // $(function () {
+    //     $(".menu_category ul li").click(function (e) {
+    //         e.preventDefault();
+    //         $(".menu_category ul li").removeClass('active');
+    //         $(this).addClass('active');
+    //     })
+    // });
+
+    // Active link
+    $(function () {
+        let location = window.location.href;
+        $('.main_menu li a').toArray().forEach(function(el) {
+            let link = $(el).prop('href');
+            if (location === link) {
+                $(el).addClass('active2');
+            }
+
+        });
+    });
+
+    // Show more button
+    $(".adv_sm_btn").click(function () {
+        let $button = $(this);
+        let $cont = $button.siblings('.main_gallery');
+        $cont.toggleClass('visible_g').toggleClass('hidden_g');
+        $button.text($button.text() === "Показать больше" ? "Закрыть": "Показать больше");
+        $(this).toggleClass('adv_sm_btn adv_sm_btn2');
+    });
+    // Blog buttons
+    $(".s_m").click(function () {
+        let $button1 = $(this);
+        let $cont1 = $button1.siblings('.more_posts');
+        $cont1.toggleClass('visible_g').toggleClass('hidden_g');
+        // $button.text($button.text() === "Показать больше" ? "Закрыть": "Показать больше");
+    });
+    //Vacancy buttons
+    $('.t_btn').click(function () {
+        let $vbtn = $(this);
+        let $vcont =$vbtn.siblings('.vacancy_item');
+        $vcont.slideToggle(500);
+    })
+
+    // Category/image switcher
+
+
+    $('.menu_category_bnt1').on({
+        'mouseover': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Футболки</li><li>Сумки и рюкзаки</li><li>Униформа</li><li>Флисовые кофты</li><li>Кепки и бейсболки</li><li>Жилетки и куртки</li><li>Фартуки</li></ul>')
+        }
+    });
+
+    $('.menu_category_bnt2').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/yellow.jpg');
+            $('.menu_subcategory').html('<ul><li>Объёмные буквы</li><li>Вывески</li><li>Крышные конструкции</li><li>Лайтбоксы/Световые короба</li><li>Оформление витрин</li><li>Обшивка фасада композитом</li><li>Информационные таблички</li><li>Оформление входных групп и козырьков</li><li>Рекламные конструкции</li><li>Брендирование транспорта</li><li>Уличные флажные конструкции</li><li>LED Дисплеи</li></ul>')
+        }
+    });
+
+    $('.menu_category_bnt3').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/red.jpg');
+            $('.menu_subcategory').html('<ul><li>Печать на баннере</li><li>Печать на пленке</li><li>Печать на баннерной сетке</li><li>Печать на холсте</li><li>Печать на бумаге</li></ul>')
+        }
+    });
+
+    $('.menu_category_bnt4').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/black.jpg');
+            $('.menu_subcategory').html('<ul><li>Системы визуальной навигации</li><li>Стойки Ресепшн</li><li>Световой логотип</li><li>Системы освещения</li><li>Изготовление фотозон</li><li>Уголки покупателя</li><li>Изготовление часов</li><li>Оформление витрин</li><li>Оформление торгового зала</li><li>Стенды для образцов</li><li>Информационный стенд</li><li>Буквы из пенопласта</li><li>Рекламные конструкции</li><li>Оформление стеклянных перегородок</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt5').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Ручки</li><li>Аксессуары </li><li>Антистрессы</li><li>Брелки</li><li>Флаги и флажки</li><li>ЭКО изделия из дерева и фанеры</li><li>Воздушные шары</li><li>Пакеты и упаковка</li><li>Чашки и посуда</li><li>Флешки и PowerBank</li><li>Ароматизаторы в автомобиль</li><li>Магниты</li><li>Зонты</li><li>Изготовление часов</li><li>Шоколадки</li><li>Значки</li><li>Дисконтные карты/удостоверения</li><li>Ежедневники</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt6').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Визитки</li><li>Буклеты/Лифлеты/листовки</li><li>Ежедневники</li><li>Блокноты</li><li>Каталоги и брошюры</li><li>Упаковка</li><li>Плакаты</li><li>Календари</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt7').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Разработка корпоративного стиля</li><li>Брендирование транспорта</li><li>Полиграфия</li><li>Сувенирная продукция</li><li>Брендбук</li><li>Упаковка</li><li>Оформление выставок</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt8').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Люстры</li><li>Световые короба (Лайтбоксы)</li><li>Архитектурная подсветка</li><li>Освещение витрин</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt9').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/yellow.jpg');
+            $('.menu_subcategory').html('<ul><li>Топперы</li><li>Воблеры</li><li>Изделия из акрила и оргстекла</li><li>Подставки под товар</li><li>Шелфтокеры</li><li>Стопперы</li><li>Холдеры</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt10').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Портативный стенд ROLL-UP</li><li>Портативный стенд X-BANNER (паук)</li><li>Стенд POP UP</li><li>Промостойка</li><li>Выставочный стенд</li><li>Разборной стенд для баннера</li><li>Фотозона</li><li>Штендеры</li><li>Стенды для образцов</li><li>Информационный стенд</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt11').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Изготовление мебели из металла и дерева</li><li>Часы и предметы интерьера</li><li>Элементы освещения</li><li>Нестандартные рекламные конструкции</li><li>Цветодинамические дисплеи</li></ul>');
+        }
+    });
+
+    $('.menu_category_bnt12').on({
+        'click': function(){
+            $('.change-image').attr('src','src/img/blue.jpg');
+            $('.menu_subcategory').html('<ul><li>Лазерная резка</li><li>Лазерная гравировка</li><li>Дизайн</li><li>Маркетинг</li><li>3D - Печать</li><li>Плоттерная резка</li><li>Высотные работы</li><li>Изделия из фанеры</li><li>Вышивка</li><li>Шелкотрафаретная печать</li><li>Ультрафиолетовая печать </li><li>Оформление выставок </li></ul>');
+        }
+    });
+});
